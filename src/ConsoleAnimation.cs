49f1d71d149a9d5f12c9693b3688775167be940d
Derek Gooding <djgooding@live.com>
2026-02-25 15:07:13 -0500

    [New Feature] CLI call

--- src/ConsoleAnimation.cs
+++ src/ConsoleAnimation.cs
@@ -5,67 +5,80 @@ namespace Animated_HelloWord;
 
 internal class ConsoleAnimation
 {
-    public readonly string AnimationText;
+    public ConsoleAnimation(string text)
+    {
+        _animationText = FiggleFonts.Larry3d.Render(text);
+        ForegroundColor = ConsoleColor.DarkBlue;
+    }
 
+    private readonly string _animationText;
     private CursorPositon _cursorPosition = new(0, 0);
 
     public static readonly int ColorCount = Enum.GetNames<ConsoleColor>().Length;
 
-    public ConsoleAnimation(string text)
+    internal void AnimateByDirection(AnimationType type)
     {
-        AnimationText = FiggleFonts.Larry3d.Render(text);
-        ForegroundColor = ConsoleColor.DarkBlue;
-    }
-
-    internal void AnimateByDirection(AnimationType Animation = AnimationType.RightToLeft)
-    {
-        if (Animation is AnimationType.LeftToRight or AnimationType.RightToLeft)
+        if (type is AnimationType.LeftToRight or AnimationType.RightToLeft)
         {
-            var lines = AnimationText.Split("\n");
-            var maxWidth = lines.Max(l => l.Length);
-
-            foreach (var col in (Animation == AnimationType.LeftToRight) ? Enumerable.Range(0, maxWidth) : Enumerable.Range(0, maxWidth).Reverse())
-            {
-                for (var row = 0; row < lines.Length; row++)
-                {
-                    if (col >= lines[row].Length)
-                        continue;
-
-                    _cursorPosition = new(col, row);
-                    SetCursorPosition(_cursorPosition.X, _cursorPosition.Y);
-
-                    Write(lines[row][col].ToString());
-
-                }
-
-                Thread.Sleep(1);
-
-            }
+            HandleHorizontal(type);
         }
         else
         {
-            var Lines = Animation == AnimationType.TopToBottom ? AnimationText.Split("\n") : [.. AnimationText.Split("\n").Reverse()];
-            _cursorPosition.Y = (Animation == AnimationType.TopToBottom ? 0 : Lines.Length - 1);
-
-            foreach (var line in Lines)
-            {
-                _cursorPosition.X = 0;
-                SetCursorPosition(_cursorPosition.X, _cursorPosition.Y);
-
-                foreach (var c in line.ToList())
-                {
-                    SetCursorPosition(_cursorPosition.X, _cursorPosition.Y);
-                    Write(c.ToString());
-
-                    _cursorPosition.X++;
-                }
-
-                Thread.Sleep(20);
-
-                _cursorPosition.Y += (Animation == AnimationType.TopToBottom ? 1 : -1);
-            }
+            HandleVertical(type);
         }
         ForegroundColor = NextColor();
+    }
+
+    private void HandleHorizontal(AnimationType type)
+    {
+        var lines = _animationText.Split("\n");
+        var maxWidth = lines.Max(l => l.Length);
+
+        foreach (var col in (type == AnimationType.LeftToRight)
+            ? Enumerable.Range(0, maxWidth)
+            : Enumerable.Range(0, maxWidth).Reverse())
+        {
+            for (var row = 0; row < lines.Length; row++)
+            {
+                if (col >= lines[row].Length)
+                    continue;
+
+                _cursorPosition = new(col, row);
+                SetCursorPosition(_cursorPosition.X, _cursorPosition.Y);
+
+                Write(lines[row][col].ToString());
+
+            }
+
+            Thread.Sleep(1);
+
+        }
+    }
+    private void HandleVertical(AnimationType type)
+    {
+        var Lines = type == AnimationType.TopToBottom
+            ? _animationText.Split("\n")
+            : [.. _animationText.Split("\n").Reverse()];
+
+        _cursorPosition.Y = type == AnimationType.TopToBottom ? 0 : Lines.Length - 1;
+
+        foreach (var line in Lines)
+        {
+            _cursorPosition.X = 0;
+            SetCursorPosition(_cursorPosition.X, _cursorPosition.Y);
+
+            foreach (var c in line.ToList())
+            {
+                SetCursorPosition(_cursorPosition.X, _cursorPosition.Y);
+                Write(c.ToString());
+
+                _cursorPosition.X++;
+            }
+
+            Thread.Sleep(20);
+
+            _cursorPosition.Y += type == AnimationType.TopToBottom ? 1 : -1;
+        }
     }
 
     private static ConsoleColor NextColor()
--- src/Program.cs
+++ src/Program.cs
@@ -2,14 +2,16 @@
 
 internal static class Program
 {
-    internal static void Main()
+    internal static void Main(string[] args)
     {
-        var helloWorldAnimation = new ConsoleAnimation("Hello World!");
+        var text = args.Length == 0 ? "Hello World!" : args[0];
+        var helloWorldAnimation = new ConsoleAnimation(text);
         Console.CursorVisible = false;
 
         while (true)
         {
-            helloWorldAnimation.AnimateByDirection((AnimationType)Random.Shared.Next(0, 4));
+            var direction = (AnimationType)Random.Shared.Next(0, 4);
+            helloWorldAnimation.AnimateByDirection(direction);
         }
     }
 }
